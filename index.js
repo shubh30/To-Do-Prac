79ec9a076cfc2c2168c80e7401748d6f8ae33c48
Shubham <shubhamnagpal30@gmail.com>
2020-08-06 16:35:41 +0530

    Add lodash to prevent case sensitive params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 
@@ -54,7 +55,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:customListName', function (req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
   List.findOne({ name: customListName }, function (err, result) {
     if (err) {
       console.log(err);
